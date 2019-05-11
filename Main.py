976f7c7f8bbeb9bf06d72e9ea658643bfb971123
Nick <nikita.veliev@skoltech.ru>
2019-05-12 02:30:54 +0300

    Chunk size
    Chunk size optimization iteration.

--- Main.py
+++ Main.py
@@ -25,7 +25,7 @@ strat = 'none'
 # Simulation parameters
 step = int(100)   # seconds
 simtime = int(2592100)   # seconds
-steps = int(simtime/step)
+tss = int(simtime/step)
 
 # CREATE A NEW INSTANCE OF SIMULATION
 sim = Classes.Simulation(mass, vol, 40, alt, 0.075,
@@ -44,13 +44,13 @@ print('Prepared for simulation in {}'.format(dt.now().strftime("%H:%M:%S")))
 # =============================================================================
 if __name__ == '__main__':
     cpus = os.cpu_count()
-    pl = mp.Pool()   # Create the pool object
-    args = range(int(simtime/step))   # The amount of steps as a step array
+    pl = mp.Pool(cpus)   # Create the pool object
+    args = range(tss)   # The amount of steps as a step array
 
     data = []
     # Create a proress bar
     for i in tqdm.tqdm(pl.imap_unordered(sim.step_sim, args,
-                                         int(steps/cpus**2 + 1)), total=steps):
+                                         int(tss/(2*cpus) + 1)), total=tss):
         data.append(i)
 
     # Close & join the pool to commit the operations on multiprocessing
